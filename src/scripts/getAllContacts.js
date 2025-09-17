73211696d08c8170f0e53bb989d2a1402f881d61
Caner <canerkesici08@gmail.com>
2025-09-17 22:00:34 +0300

    ilk odev bitti

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,11 @@
-export const getAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const getAllContacts = async () => {
+
+   return await readContacts(); // db.json'dan diziyi oku
+
+
+};
 
 console.log(await getAllContacts());
+
